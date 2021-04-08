29c95c52a3be2ff1f2d3165c5a474a472660128e
William Wold <wm@wmww.sh>
2021-04-08 09:30:13 -0700

    Mir: auto-select clang or gcc build

--- repos/mir/run_cmake.sh
+++ repos/mir/run_cmake.sh
@@ -6,8 +6,20 @@ INSTALL="$DIR/install"
 mkdir -p "$INSTALL"
 
 if test -z "$CC"; then
-    export CC=clang
+    # If the build directory has "clang" in the name default to Clang, if "gcc" then default to GCC
+    BUILD_DIR=$(basename $(readlink -f $(pwd)))
+    if echo "$BUILD_DIR" | grep -i clang >/dev/null; then
+        export CC=clang
+        echo "In $BUILD_DIR so using Clang"
+    elif echo "$BUILD_DIR" | grep -i gcc >/dev/null; then
+        export CC=gcc
+        echo "In $BUILD_DIR so using GCC"
+    else
+        export "Letting CMake choose the compiler"
+    fi
+else
+    echo "CC is already set to $CC"
 fi
 
-cmake -DMIR_USE_LD=lld -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="$INSTALL" $@
+cmake "$DIR" -DMIR_USE_LD=lld -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX="$INSTALL" $@
 
