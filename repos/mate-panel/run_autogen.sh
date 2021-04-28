7ae1aea041a9c9f763c99109819fd343f06b78a4
William Wold <wm@wmww.sh>
2021-04-28 11:05:16 -0700

    mate-panel repo: install to subdirectory

--- repos/mate-panel/run_autogen.sh
+++ repos/mate-panel/run_autogen.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+INSTALL="$DIR/install"
+mkdir -p "$INSTALL"
 export CFLAGS="-Wno-deprecated-declarations -Werror -g -O0 $CFLAGS"
-./autogen.sh --with-in-process-applets=all --prefix ~/code/install/usr/local/ $@
+"$DIR/autogen.sh" --with-in-process-applets=all --prefix "$INSTALL" $@
