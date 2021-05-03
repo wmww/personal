64915883ea30e4f357450be10d026eab19866ed0
William Wold <wm@wmww.sh>
2021-05-03 11:03:01 -0700

    Mir: fix clean_pch.sh

--- repos/mir/clean_pch.sh
+++ repos/mir/clean_pch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+set -eox pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-find "$DIR" -name '*.pch' | xargs rm
+find "$DIR" -name '*.pch' -o -name '*.gch' | xargs rm
