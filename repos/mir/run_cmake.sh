d16f2fd0a186ca8e28c7bcbcb643425802694e7b
William Wold <wm@wmww.sh>
2021-05-04 13:28:15 -0700

    Mir: don't run cmake in source dir

--- repos/mir/run_cmake.sh
+++ repos/mir/run_cmake.sh
@@ -5,6 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 INSTALL="$DIR/install"
 mkdir -p "$INSTALL"
 
+if test $(dirname $PWD) != $DIR; then
+    echo "You are not in a subdir of $DIR, this is probably a mistake"
+    exit 1
+fi
+
 if test -z "$CC"; then
     # If the build directory has "clang" in the name default to Clang, if "gcc" then default to GCC
     BUILD_DIR=$(basename $(readlink -f $(pwd)))
@@ -15,7 +20,7 @@ if test -z "$CC"; then
         export CC=gcc
         echo "In $BUILD_DIR so using GCC"
     else
-        export "Letting CMake choose the compiler"
+        echo "Letting CMake choose the compiler"
     fi
 else
     echo "CC is already set to $CC"
