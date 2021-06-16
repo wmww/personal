f164496a628b1c1944f57a9aa56fd8a8393abb6f
William Wold <wm@wmww.sh>
2021-06-15 17:51:50 -0700

    WLCS: add more tests to exclude

--- repos/wlcs/run_against_mir.sh
+++ repos/wlcs/run_against_mir.sh
@@ -2,4 +2,10 @@
 set -euox pipefail
 
 cmake --build build
+
+if test ! -e exclude.txt; then
+    echo 'exclude.txt not found'
+    exit 1
+fi
+
 exec ./build/wlcs ../mir/build/lib/miral_wlcs_integration.so --gtest_filter="-$(cat exclude.txt)" $@
