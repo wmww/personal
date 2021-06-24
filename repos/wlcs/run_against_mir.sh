ae1cc0b18b1b872d241800a1806f0e1b691556c1
William Wold <wm@wmww.sh>
2021-06-23 17:38:59 -0700

    WLCS run_against_mir.sh: use Mir test exclusion list

--- repos/wlcs/run_against_mir.sh
+++ repos/wlcs/run_against_mir.sh
@@ -3,9 +3,10 @@ set -euox pipefail
 
 cmake --build build
 
-if test ! -e exclude.txt; then
-    echo 'exclude.txt not found'
-    exit 1
-fi
+# This was hacked together from 3 confusing SO answers, I have no idea how it works
+FILTER=$(cat ../mir/tests/acceptance-tests/wayland/CMakeLists.txt | sed -n "/^set(EXPECTED_FAILURES$/,/)/ { /^set(EXPECTED_FAILURES$/d ; /)/d ; /^$/d ; p }" | sed 's/#.*$//g; s/\s//g' | sed -z 's/\n/:/g')
 
-exec ./build/wlcs ../mir/build/lib/miral_wlcs_integration.so --gtest_filter="-$(cat exclude.txt)" $@
+# These tests take a while and are annoying
+FILTER="$FILTER:*times_out*"
+
+exec ./build/wlcs ../mir/build/lib/miral_wlcs_integration.so --gtest_filter="-$FILTER" $@
