5c49804c8fdc8779c5c74068e7dd2fd069523b09
William Wold <wm@wmww.sh>
2021-07-19 15:00:36 -0700

    Mir: fix clean_pch.sh

--- repos/mir/clean_pch.sh
+++ repos/mir/clean_pch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-set -eox pipefail
+set -eo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-find "$DIR" -name '*.pch' -o -name '*.gch' | xargs rm
+DIR="$(realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
+PCH=$(find "$DIR" -name '*.pch' -o -name '*.gch')
+if test -z "$PCH"; then
+    echo "No files to remove"
+else
+    echo "Removing $PCH"
+    rm $PCH
+fi
+
