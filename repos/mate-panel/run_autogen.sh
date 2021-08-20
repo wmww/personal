a41e7157f836553629c75b92079ba1ff9d90ed2a
William Wold <wm@wmww.sh>
2021-08-20 09:48:21 -0700

    mate-panel: drop -Werror

--- repos/mate-panel/run_autogen.sh
+++ repos/mate-panel/run_autogen.sh
@@ -3,5 +3,5 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 INSTALL="$DIR/install"
 mkdir -p "$INSTALL"
-export CFLAGS="-Wno-deprecated-declarations -Werror -g -O0 $CFLAGS"
+export CFLAGS="-Wno-deprecated-declarations -g -O0 $CFLAGS"
 "$DIR/autogen.sh" --with-in-process-applets=all --prefix "$INSTALL" $@
