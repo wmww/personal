a3b6ca667a254e83c086c0f39bab53db83dfa43c
William Wold <wm@wmww.sh>
2021-08-20 09:56:28 -0700

    mate-panel: document scripts

--- repos/mate-panel/run_autogen.sh
+++ repos/mate-panel/run_autogen.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+# Set the script to abort on error
 set -e
+
+# Create an "install" directory next to where the script is that the panel will be installed into
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 INSTALL="$DIR/install"
 mkdir -p "$INSTALL"
+
+# Run autogen.sh with the following options:
+# - -Wno-deprecated-declarations: this supresses a bunch of useless warnings
+# - -g -O0: make debugging easy
+# - --with-in-process-applets=all: out-of-process applets don't work on Wayland
+# - --prefix "$INSTALL": `make install` should install to the directory we made above
+# - $@: forward any additional arguments passed to this script
 export CFLAGS="-Wno-deprecated-declarations -g -O0 $CFLAGS"
 "$DIR/autogen.sh" --with-in-process-applets=all --prefix "$INSTALL" $@
--- repos/mate-panel/run_panel.sh
+++ repos/mate-panel/run_panel.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+# Set the script to abort on error
 set -e
 
 # to build the panel:
 # ./run_autogen.sh
 # make && make install
 
+# Get the path to the install directory next to this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 INSTALL="$DIR/install"
 
+# Run the panel using the gsettings schema from the install directory
 GSETTINGS_SCHEMA_DIR="$INSTALL/share/glib-2.0/schemas/" "$INSTALL/bin/mate-panel"
