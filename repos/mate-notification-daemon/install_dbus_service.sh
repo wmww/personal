4f6c0f148660904c9f475093d3d548ee79981ab3
William Wold <wm@wmww.sh>
2021-09-03 13:18:35 -0700

    Modernize mate-notification-daemon scripts

--- repos/mate-notification-daemon/install_dbus_service.sh
+++ repos/mate-notification-daemon/install_dbus_service.sh
@@ -1,1 +1,5 @@
-cp /home/wmww/code/install/usr/local/share/dbus-1/services/org.freedesktop.mate.Notifications.service /usr/share/dbus-1/services/org.freedesktop.mate.Notifications.service
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+INSTALL="$DIR/install"
+cp "$INSTALL/share/dbus-1/services/org.freedesktop.mate.Notifications.service" "/usr/share/dbus-1/services/org.freedesktop.mate.Notifications.service"
--- repos/mate-notification-daemon/run_autogen.sh
+++ repos/mate-notification-daemon/run_autogen.sh
@@ -1,1 +1,14 @@
-./autogen.sh --prefix ~/code/install/usr/local/ --enable-x11 --enable-wayland
+#!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+INSTALL="$DIR/install"
+mkdir -p "$INSTALL"
+MATE_PANEL_INC_DIR="$DIR/../mate-panel/install/include/mate-panel-4.0/libmate-panel-applet/"
+export CFLAGS="-Wno-deprecated-declarations -I$MATE_PANEL_INC_DIR $CFLAGS"
+# export CFLAGS="-Wno-deprecated-declarations -Werror -g -O0 $CFLAGS"
+"$DIR/autogen.sh" --enable-x11 --enable-wayland --prefix "$INSTALL" $@
+if test ! -f "$MATE_PANEL_INC_DIR/mate-panel-applet.h"; then
+    echo
+    echo "    WARNING: $MATE_PANEL_INC_DIR/mate-panel-applet.h does not exist, build may be broken!"
+    echo
+fi
