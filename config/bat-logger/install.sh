d7ac202d33db53965f6df051ba6db0a55cc7f563
William Wold <wm@wmww.sh>
2022-02-28 10:32:52 -0700

    Auto-start bat-logger on install

--- config/bat-logger/install.sh
+++ config/bat-logger/install.sh
@@ -4,4 +4,5 @@ set -euox pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cp "$DIR/bat-logger.service" "/etc/systemd/system/"
 cp "$DIR/bat-logger.py" "/usr/local/bin/"
+systemctl enable --now bat-logger
 systemctl status bat-logger
